b3b3d7f2315b9ecf6a35b27cb0b089cf0766aa54
Robert Barrow <merefield@gmail.com>
2020-06-18 12:40:45 +0100

    update mime gem dependencies so they are compatible with my other plugins

--- plugin.rb
+++ plugin.rb
@@ -10,8 +10,8 @@ gem 'websocket', '1.2.8'
 gem 'websocket-client-simple', '0.3.0'
 gem 'opus-ruby', '1.0.1', { require: false }
 gem 'netrc', '0.11.0'
-gem 'mime-types-data', '3.2018.0812'
-gem 'mime-types', '3.2.2'
+gem 'mime-types-data', '3.2019.1009'
+gem 'mime-types', '3.3.1'
 gem 'domain_name', '0.5.20180417'
 gem 'http-cookie','1.0.3'
 gem 'http-accept', '1.7.0', { require: false }

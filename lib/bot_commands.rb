b018a0e83ddf6dc12915967ff192c3d94b4ec849
Robert Barrow <merefield@gmail.com>
2021-09-01 19:34:24 +0100

    FEATURE: support associated accounts: show correct user for message if user has account in Discourse

--- lib/bot_commands.rb
+++ lib/bot_commands.rb
@@ -28,22 +28,31 @@ module ::DiscordBot::BotCommands
         end
       end
       system_user = User.find_by(id: -1)
+
       total_copied_messages = 0
-      # .sort_by(|m| -m.id)
+
       past_messages.reverse.in_groups_of(SiteSetting.discord_bot_message_copy_topic_size_limit.to_i).each_with_index do |message_batch, index|
         current_topic_id = nil
         message_batch.each_with_index do |pm, topic_index|
           next if pm.nil?
           raw = pm.to_s
+
+          associated_user = UserAssociatedAccount.find_by(provider_uid: pm.author.id)
+          unless associated_user.nil?
+            posting_user = User.find_by(id: associated_user.user_id)
+          else
+            posting_user = system_user
+          end
+
           if topic_index == 0 && destination_topic.nil?
-            new_post = PostCreator.create!(system_user, title: I18n.t("discord_bot.commands.disccopy.discourse_topic_title", channel: event.channel.name) + (past_messages.count <= SiteSetting.discord_bot_message_copy_topic_size_limit ? "" : " #{index + 1}") , raw: raw, category: destination_category.id, skip_validations: true)
+            new_post = PostCreator.create!(posting_user, title: I18n.t("discord_bot.commands.disccopy.discourse_topic_title", channel: event.channel.name) + (past_messages.count <= SiteSetting.discord_bot_message_copy_topic_size_limit ? "" : " #{index + 1}") , raw: raw, category: destination_category.id, skip_validations: true)
             total_copied_messages += 1
             current_topic_id = new_post.topic.id
           elsif !destination_topic.nil? || !current_topic_id.nil?
             if current_topic_id.nil?
               current_topic_id = destination_topic.id
             end
-            new_post = PostCreator.create!(system_user, raw: raw, topic_id: current_topic_id, skip_validations: true)
+            new_post = PostCreator.create!(posting_user, raw: raw, topic_id: current_topic_id, skip_validations: true)
             total_copied_messages += 1
           else 
             event.respond I18n.t("discord_bot.commands.disccopy.error.unable_to_determine_topic_id")
--- lib/discord_events_handlers.rb
+++ lib/discord_events_handlers.rb
@@ -3,23 +3,31 @@ module ::DiscordBot::DiscordEventsHandlers
   module TransmitAnnouncement
     extend Discordrb::EventContainer
     message do |event|
-      # Copy the message to the assigned Discourse announcement Topic if assigned in plugin settings
-      discourse_announcement_topic = Topic.find_by(id: SiteSetting.discord_bot_discourse_announcement_topic_id)
-      unless discourse_announcement_topic.nil?
-        system_user = User.find_by(id: -1)
-        raw = event.message.to_s
-        new_post = PostCreator.new(system_user, raw: raw, topic_id: discourse_announcement_topic.id)
-        new_post.create!
+
+      system_user = User.find_by(id: -1)
+
+      associated_user = UserAssociatedAccount.find_by(provider_uid: event.message.author.id)
+      unless associated_user.nil?
+        posting_user = User.find_by(id: associated_user.user_id)
+      else
+        posting_user = system_user
       end
+
       if SiteSetting.discord_bot_auto_channel_sync
         matching_category = Category.find_by(name: event.message.channel.name)
         unless matching_category.nil?
           raw = event.message.to_s
-          system_user = User.find_by(id: -1)
           if !(target_topic = Topic.find_by(title: I18n.t("discord_bot.discord_events.auto_message_copy.default_topic_title", channel_name: matching_category.name))).nil?
-            new_post = PostCreator.create!(system_user, raw: raw, topic_id: target_topic.id, skip_validations: true)
+            new_post = PostCreator.create!(posting_user, raw: raw, topic_id: target_topic.id, skip_validations: true)
           else
-            new_post = PostCreator.create!(system_user, title: I18n.t("discord_bot.discord_events.auto_message_copy.default_topic_title", channel_name: matching_category.name), raw: raw, category: matching_category.id, skip_validations: true)
+            new_post = PostCreator.create!(posting_user, title: I18n.t("discord_bot.discord_events.auto_message_copy.default_topic_title", channel_name: matching_category.name), raw: raw, category: matching_category.id, skip_validations: true)
+          end
+        else
+          # Copy the message to the assigned Discourse announcement Topic if assigned in plugin settings
+          discourse_announcement_topic = Topic.find_by(id: SiteSetting.discord_bot_discourse_announcement_topic_id)
+          unless discourse_announcement_topic.nil?
+            raw = event.message.to_s
+            new_post = PostCreator..create!(posting_user, raw: raw, topic_id: discourse_announcement_topic.id)
           end
         end
       end

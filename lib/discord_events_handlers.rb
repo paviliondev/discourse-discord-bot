68b9b43fb3e67504d859f34e4c0e86c1e534df6b
merefield <merefield@gmail.com>
2021-10-26 09:44:33 +0100

    lib/discord_events_handlers.rb

--- lib/discord_events_handlers.rb
+++ lib/discord_events_handlers.rb
@@ -13,24 +13,28 @@ module ::DiscordBot::DiscordEventsHandlers
         posting_user = system_user
       end
 
-      if SiteSetting.discord_bot_auto_channel_sync
-        matching_category = Category.find_by(name: event.message.channel.name)
-        raw = event.message.to_s
-        if !raw.blank?
+      raw = event.message.to_s
+      if !raw.blank?
+        if SiteSetting.discord_bot_auto_channel_sync
+          matching_category = Category.find_by(name: event.message.channel.name)
           unless matching_category.nil?
             if !(target_topic = Topic.find_by(title: I18n.t("discord_bot.discord_events.auto_message_copy.default_topic_title", channel_name: matching_category.name))).nil?
               new_post = PostCreator.create!(posting_user, raw: raw, topic_id: target_topic.id, skip_validations: true)
             else
               new_post = PostCreator.create!(posting_user, title: I18n.t("discord_bot.discord_events.auto_message_copy.default_topic_title", channel_name: matching_category.name), raw: raw, category: matching_category.id, skip_validations: true)
             end
-          else
-            # Copy the message to the assigned Discourse announcement Topic if assigned in plugin settings
-            discourse_announcement_topic = Topic.find_by(id: SiteSetting.discord_bot_discourse_announcement_topic_id)
-            unless discourse_announcement_topic.nil?
-              new_post = PostCreator..create!(posting_user, raw: raw, topic_id: discourse_announcement_topic.id)
-            end
+            return
           end
         end
+        if_set_send_to_announcement_topic(posting_user, raw)
+      end
+    end
+
+    def if_set_send_to_announcement_topic(posting_user, raw)
+      # Copy the message to the assigned Discourse announcement Topic if assigned in plugin settings
+      discourse_announcement_topic = Topic.find_by(id: SiteSetting.discord_bot_discourse_announcement_topic_id)
+      unless discourse_announcement_topic.nil?
+        new_post = PostCreator..create!(posting_user, raw: raw, topic_id: discourse_announcement_topic.id)
       end
     end
   end

31c60f6d8769b503640c8de83a84dcbeef878739
merefield <merefield@gmail.com>
2023-06-20 08:04:53 +0100

    cutting code

--- plugin.rb
+++ plugin.rb
@@ -5,8 +5,15 @@
 # authors: Robert Barrow
 # url: https://github.com/merefield/discourse-discord-bot
 
+
+libdir = File.join(File.dirname(__FILE__), "vendor/discordrb/lib")
+
+pp $LOAD_PATH.count
+$LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
+pp $LOAD_PATH.count
+
 gem 'event_emitter', '0.2.6'
-gem 'websocket', '1.2.8'
+gem 'websocket', '1.2.9'
 gem 'websocket-client-simple', '0.3.0'
 gem 'opus-ruby', '1.0.1', { require: false }
 gem 'netrc', '0.11.0'
@@ -18,9 +25,11 @@ gem 'http-accept', '1.7.0', { require: false }
 gem 'rest-client', '2.1.0.rc1'
 
 gem 'discordrb-webhooks', '3.3.0', { require: false }
-gem 'discordrb', '3.4.0'
+#gem 'discordrb', :path => "/vendor/discordrb"
 
-require 'discordrb'
+
+
+require_relative 'vendor/discordrb/lib/discordrb.rb'
 
 enabled_site_setting :discord_bot_enabled
 

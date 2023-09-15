e3248a852583d1bf82f7dc6883ef56b168395ca7
merefield <merefield@gmail.com>
2023-09-15 15:12:35 +0100

    debug

--- lib/bot_commands.rb
+++ lib/bot_commands.rb
@@ -123,7 +123,9 @@ module ::DiscordBot::BotCommands
           end
         end
         event.respond I18n.t("discord_bot.commands.disccopy.success.final_outcome", count: total_copied_messages)
+        event.respond "hello"
         url = "#{Discourse.current_hostname}/t/slug/#{current_topic_id}"
+        event.respond url
         event.respond I18n.t("discord_bot.commands.disccopy.success.link", url: url)
       end
     end

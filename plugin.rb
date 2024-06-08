541f9dd11c3075bfb1f9a2aaa0dab370119b656f
merefield <merefield@gmail.com>
2024-06-08 08:24:32 +0100

    IMPROVE: utilise embed info better

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-discord-bot
 # about: Integrate Discord Bots with Discourse
-# version: 0.3.12
+# version: 0.3.13
 # authors: Robert Barrow
 # url: https://github.com/merefield/discourse-discord-bot
 

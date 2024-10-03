5c707044dcf884f01bdb0c74f026a9c8b79ccd2e
merefield <merefield@gmail.com>
2024-10-03 07:49:10 +0100

    dry up Post prep code

--- lib/discord_events_handlers.rb
+++ lib/discord_events_handlers.rb
@@ -9,44 +9,16 @@ module ::DiscordBot::DiscordEventsHandlers
         next if !SiteSetting.discord_bot_auto_channel_sync && SiteSetting.discord_bot_discourse_announcement_topic_id.blank &&
           (event.message.channel.id.to_s != SiteSetting.discord_bot_announcement_channel_id)
 
-        system_user = User.find_by(id: -1)
+        posting_user, raw = ::DiscordBot::Utils.prepare_post(event.message)
 
-        proxy_account = User.find_by(name: SiteSetting.discord_bot_unknown_user_proxy_account)
-
-        associated_user = UserAssociatedAccount.find_by(provider_uid: event.message.author.id, provider_name: 'discord')
-
-        if associated_user.nil?
-          posting_user = proxy_account.nil? ? system_user : proxy_account
-        else
-          posting_user = User.find_by(id: associated_user.user_id)
-        end
-
-        raw = event.message.to_s
-        embed = event.message.embeds[0]
-        if !embed.blank?
-          url = embed.url
-          thumbnail_url = embed.thumbnail&.url || embed.image&.url || embed.video&.url || ""
-          description = embed.description
-          title = embed.title
-        end
-
-        event.message.attachments.each do |attachment|
-          if attachment.content_type.include?("image")
-            raw = !raw.blank? ? raw + "\n\n" + attachment.url : attachment.url
-          else
-            raw = !raw.blank? ? raw + "\n\n<a href='#{attachment.url}'>#{attachment.filename}</a>" : "<a href='#{attachment.url}'>#{attachment.filename}</a>"
-          end
-        end
-
-        if !raw.blank? || !embed.nil?
-          content = raw.blank? ? I18n.t("discord_bot.discord_events.auto_message_copy.embed", url: url, description: description, title: title, thumbnail_url: thumbnail_url) : raw
+        if !raw.blank?
           if SiteSetting.discord_bot_auto_channel_sync
             matching_category = Category.find_by(name: event.message.channel.name)
             unless matching_category.nil?
               if !(target_topic = Topic.find_by(title: I18n.t("discord_bot.discord_events.auto_message_copy.default_topic_title", channel_name: matching_category.name))).nil?
-                new_post = PostCreator.create!(posting_user, raw: content, topic_id: target_topic.id, skip_validations: true)
+                new_post = PostCreator.create!(posting_user, raw: raw, topic_id: target_topic.id, skip_validations: true)
               else
-                new_post = PostCreator.create!(posting_user, title: I18n.t("discord_bot.discord_events.auto_message_copy.default_topic_title", channel_name: matching_category.name), raw: content, category: matching_category.id, skip_validations: true)
+                new_post = PostCreator.create!(posting_user, title: I18n.t("discord_bot.discord_events.auto_message_copy.default_topic_title", channel_name: matching_category.name), raw: raw, category: matching_category.id, skip_validations: true)
               end
               return
             end
@@ -55,7 +27,7 @@ module ::DiscordBot::DiscordEventsHandlers
             # Copy the message to the assigned Discourse announcement Topic if assigned in plugin settings
             discourse_announcement_topic = Topic.find_by(id: SiteSetting.discord_bot_discourse_announcement_topic_id.to_i)
             unless discourse_announcement_topic.nil?
-              new_post = PostCreator.create!(posting_user, raw: content, topic_id: discourse_announcement_topic.id, skip_validations: true)
+              new_post = PostCreator.create!(posting_user, raw: raw, topic_id: discourse_announcement_topic.id, skip_validations: true)
             end
           end
         end

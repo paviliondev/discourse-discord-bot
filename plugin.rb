bdd7a8cd40fc2306fb995027653a135413d671b2
merefield <merefield@gmail.com>
2024-10-03 08:05:58 +0100

    bump patch

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-discord-bot
 # about: Integrate Discord Bots with Discourse
-# version: 0.3.17
+# version: 0.3.18
 # authors: Robert Barrow
 # url: https://github.com/merefield/discourse-discord-bot
 

96945c00d56501c9a68d8dd43b4e811690bfe2fe
merefield <merefield@gmail.com>
2025-01-02 19:59:31 +0000

    IMPROVE: localize strings for Discourse notification events

--- lib/discourse_events_handlers.rb
+++ lib/discourse_events_handlers.rb
@@ -8,11 +8,11 @@ module ::DiscordBot::DiscourseEventsHandlers
         posted_category = post.topic.category.id
         posted_category_name = Category.find_by(id: posted_category).name
         if post_listening_categories.include?(posted_category.to_s) then
-          message = "There's a new Post in the '#{posted_category_name}' Category on Discourse: #{Discourse.base_url + post.url}"
+          message = I18n.t("discord_bot.discourse_events.announce_new_post", posted_category_name: posted_category_name, url: Discourse.base_url + post.base_url)
           ::DiscordBot::Bot.discord_bot.send_message(SiteSetting.discord_bot_announcement_channel_id, message)
         else
           if topic_listening_categories.include?(posted_category.to_s) && post.post_number = 1 then
-            message = "There's a new Topic in the '#{posted_category_name}' Category on Discourse: #{Discourse.base_url + post.url}"
+            message = I18n.t("discord_bot.discourse_events.announce_new_topic", posted_category_name: posted_category_name, url: Discourse.base_url + post.base_url)
             ::DiscordBot::Bot.discord_bot.send_message(SiteSetting.discord_bot_announcement_channel_id, message)
           end
         end
